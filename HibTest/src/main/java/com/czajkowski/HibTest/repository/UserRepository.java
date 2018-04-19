275019c79007c332513064bd1ef8598415021589
Czajkowski <Czajkowski@192.168.0.11>
2018-04-19 19:44:05 +0200

    cd

--- HibTest/src/main/java/com/czajkowski/HibTest/repository/UserRepository.java
+++ HibTest/src/main/java/com/czajkowski/HibTest/repository/UserRepository.java
@@ -3,6 +3,8 @@ package com.czajkowski.HibTest.repository;
 import java.util.List;
 import java.util.Optional;
 
+import javax.transaction.Transactional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Component;
@@ -15,4 +17,7 @@ public interface UserRepository extends JpaRepository <User, Long> {
 	List<User> findByFirstName(String firstName);
 	@Query("SELECT SUM(age) FROM User  u WHERE u.age > 25")
 	Optional<Integer> age();
+	@Transactional
+	void deleteByUserId(int id);
+	User findByUserId(int id);
 }
